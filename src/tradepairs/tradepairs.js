e7ea93621625c8c903518c08c440be3c29b1dcfc
Valamidev <Valamidev@users.noreply.github.com>
2019-07-22 15:28:09 +0200

    Implement candlestick-convert 2.0.0

--- src/tradepairs/tradepairs.js
+++ src/tradepairs/tradepairs.js
@@ -4,15 +4,23 @@ const _ = require("lodash");
 const util = require("../utils");
 const logger = require("../logger");
 const { pool, candle_db } = require("../database");
+const candle_convert = require("candlestick-convert");
 
 class Tradepairs {
   constructor() {}
 
   async get_candlestick(exchange, symbol, interval, limit = 0) {
     try {
-      let table_name = util.candlestick_name(exchange, symbol, interval);
+      /* TODO remove hardcoded value */
+      let table_name = util.candlestick_name(exchange, symbol, 60);
 
       let rows = [];
+
+      // Converted Candles
+      if (interval != 60 && limit != 0) {
+        // Limit should be always higher than convert ratio * 1,5 + 1
+        limit += parseInt((interval / 60) * 1.5) + 1;
+      }
 
       if (limit == 0) {
         [rows] = await candle_db.query(
@@ -30,6 +38,11 @@ class Tradepairs {
 
       // Sort by time asc
       rows = _.sortBy(rows, ["time"]);
+
+      // Convert into new timeframe
+      if (interval != 60) {
+        rows = candle_convert.json(rows, 60, interval);
+      }
 
       return rows;
     } catch (e) {

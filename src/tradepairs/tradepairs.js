0f8f3e680132ad798ffc88682f7c76b1aa55c710
Valamidev <Valamidev@users.noreply.github.com>
2019-07-25 21:17:12 +0200

    Upgrade Get Candlestick

--- src/tradepairs/tradepairs.js
+++ src/tradepairs/tradepairs.js
@@ -1,10 +1,10 @@
-"use strict";
+"use strict"
 
-const _ = require("lodash");
-const util = require("../utils");
-const logger = require("../logger");
-const { pool, candle_db } = require("../database");
-const candle_convert = require("candlestick-convert");
+const _ = require("lodash")
+const util = require("../utils")
+const logger = require("../logger")
+const { pool, candle_db } = require("../database")
+const candle_convert = require("candlestick-convert")
 
 class Tradepairs {
   constructor() {}
@@ -12,55 +12,51 @@ class Tradepairs {
   async get_candlestick(exchange, symbol, interval, limit = 0) {
     try {
       /* TODO remove hardcoded value */
-      let table_name = util.candlestick_name(exchange, symbol, 60);
+      let exchange_base_interval = 60
 
-      let rows = [];
+      let table_name = util.candlestick_name(exchange, symbol, exchange_base_interval)
+
+      let rows = []
 
       // Converted Candles
-      if (interval != 60 && limit != 0) {
+      if (interval != exchange_base_interval && limit != 0) {
+        limit *= interval / exchange_base_interval
+
         // Limit should be always higher than convert ratio * 1,5 + 1
-        limit += parseInt((interval / 60) * 1.5) + 1;
+        limit += parseInt((interval / exchange_base_interval) * 1.5) + 1
       }
 
       if (limit == 0) {
-        [rows] = await candle_db.query(
-          "SELECT * FROM `" + table_name + "` ORDER BY `time` ASC;"
-        );
+        ;[rows] = await candle_db.query("SELECT * FROM `" + table_name + "` ORDER BY `time` ASC;")
       } else {
-        [rows] = await candle_db.query(
-          "SELECT * FROM `" +
-            table_name +
-            "` ORDER BY `time` DESC LIMIT " +
-            limit +
-            ";"
-        );
+        ;[rows] = await candle_db.query("SELECT * FROM `" + table_name + "` ORDER BY `time` DESC LIMIT " + limit + ";")
       }
 
       // Sort by time asc
-      rows = _.sortBy(rows, ["time"]);
+      rows = _.sortBy(rows, ["time"])
 
       // Convert into new timeframe
-      if (interval != 60) {
-        rows = candle_convert.json(rows, 60, interval);
+      if (interval != exchange_base_interval) {
+        rows = candle_convert.json(rows, exchange_base_interval, interval)
       }
 
-      return rows;
+      return rows
     } catch (e) {
-      logger.error("SQL error", e);
+      logger.error("SQL error", e)
     }
   }
 
   async load_tradepairs() {
     try {
-      let [rows] = await pool.query("SELECT * FROM `tradepairs`;");
+      let [rows] = await pool.query("SELECT * FROM `tradepairs`;")
 
-      this.tradepairs = rows;
+      this.tradepairs = rows
 
-      return rows;
+      return rows
     } catch (e) {
-      logger.error("SQL error", e);
+      logger.error("SQL error", e)
     }
   }
 }
 
-module.exports = new Tradepairs();
+module.exports = new Tradepairs()
